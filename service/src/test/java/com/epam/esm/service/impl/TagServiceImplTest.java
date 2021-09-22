d5ed1bad260148a879ee865c17b74fc2435ae3c4
Ruslan Nedvedskiy <ruslanned2001@gmail.com>
2021-09-22 13:19:12 +0300

    tagService test added

--- service/src/test/java/com/epam/esm/service/impl/TagServiceImplTest.java
+++ service/src/test/java/com/epam/esm/service/impl/TagServiceImplTest.java
@@ -4,100 +4,146 @@ import com.epam.esm.dao.TagDao;
 import com.epam.esm.dto.TagDto;
 import com.epam.esm.entity.Tag;
 import com.epam.esm.mapper.impl.TagMapper;
+import com.epam.esm.service.TagService;
+import com.epam.esm.validator.PaginationValidator;
+import com.epam.esm.validator.TagValidator;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
 class TagServiceImplTest {
 
-    @InjectMocks
-    private TagServiceImpl tagService;
+    private TagDao tagDao;
+    private TagMapper tagMapper;
+    private PaginationValidator paginationValidator;
+    private TagValidator tagValidator;
+    private TagService tagService;
 
-    @Mock
-    TagDao tagDao;
 
-    @Mock
-    TagMapper tagMapper;
-/*
-    @Test
-    void savePositiveTest() {
-        String tagName = "tagName";
-        Tag tagExpected = new Tag();
-        TagDto tagDtoExpected = new TagDto();
-        tagExpected.setName(tagName);
-        tagDtoExpected.setName(tagName);
+    @BeforeEach
+    void setUp() {
+        tagDao = Mockito.mock(TagDao.class);
+        tagMapper = Mockito.mock(TagMapper.class);
+        paginationValidator = Mockito.mock(PaginationValidator.class);
+        tagValidator = Mockito.mock(TagValidator.class);
+        tagService =
+                new TagServiceImpl(tagDao, tagMapper, paginationValidator, tagValidator);
+    }
 
-        when(tagDao.findByName(tagName)).thenReturn(Optional.empty());
-        when(tagDao.save(tagExpected)).thenReturn(Optional.of(tagExpected));
-        when(tagMapper.mapEntityToDto(tagExpected)).thenReturn(tagDtoExpected);
-        when(tagMapper.mapDtoToEntity(tagDtoExpected)).thenReturn(tagExpected);
-        TagDto tagDtoActual = tagService.save(tagDtoExpected);
-        assertEquals(tagDtoExpected, tagDtoActual);
-        verify(tagDao, times(1)).save(tagExpected);
+    @AfterEach
+    void tearDown() {
+        tagDao = null;
+        tagMapper = null;
+        paginationValidator = null;
+        tagValidator = null;
+        tagService = null;
     }
 
     @Test
-    void findByIdPositiveTest() {
+    void save() {
         long id = 1;
-        TagDto expectedDto = new TagDto();
-        expectedDto.setName("Tag");
-        expectedDto.setId(id);
-        Tag expected = new Tag();
-        expected.setName("Tag");
+        String name = "tag1";
+        Tag tag = new Tag();
+        tag.setName(name);
+        tag.setId(id);
+        TagDto tagDto = new TagDto();
+        tagDto.setId(id);
+        tagDto.setName(name);
+        TagDto expected = new TagDto();
         expected.setId(id);
-        when(tagDao.findById(id)).thenReturn(Optional.of(expected));
-        when(tagMapper.mapEntityToDto(expected)).thenReturn(expectedDto);
-        TagDto actual = tagService.findById(id);
-        assertEquals(expectedDto, actual);
-        verify(tagDao, times(1)).findById(id);
+        expected.setName(name);
+
+        Mockito.when(tagValidator.validateTag(tagDto)).thenReturn(Collections.emptyList());
+        Mockito.when(tagMapper.mapDtoToEntity(tagDto)).thenReturn(tag);
+        Mockito.when(tagDao.save(tag)).thenReturn(Optional.of(tag));
+        Mockito.when(tagMapper.mapEntityToDto(tag)).thenReturn(tagDto);
+        TagDto actual = tagService.save(tagDto);
+
+        Assertions.assertEquals(expected, actual);
+        Mockito.verify(tagValidator, Mockito.times(1)).validateTag(tagDto);
+        Mockito.verify(tagMapper, Mockito.times(1)).mapDtoToEntity(tagDto);
+        Mockito.verify(tagDao, Mockito.times(1)).save(tag);
+        Mockito.verify(tagMapper, Mockito.times(1)).mapEntityToDto(tag);
     }
 
     @Test
-    void findAllPositiveTest() {
-        long id1 = 1;
-        long id2 = 2;
-        String tagName1 = "tag1";
-        String tagName2 = "tag2";
+    void findById() {
+        long id = 1;
+        String name = "tag1";
+        Tag tag = new Tag();
+        tag.setId(id);
+        tag.setName(name);
+        TagDto expected = new TagDto();
+        expected.setId(1);
+        expected.setName(name);
+
+        Mockito.when(tagDao.findById(id)).thenReturn(Optional.of(tag));
+        Mockito.when(tagMapper.mapEntityToDto(tag)).thenReturn(expected);
+        TagDto actual = tagService.findById(id);
+
+        Assertions.assertEquals(expected, actual);
+        Mockito.verify(tagDao, Mockito.times(1)).findById(id);
+        Mockito.verify(tagMapper, Mockito.times(1)).mapEntityToDto(tag);
+    }
+
+    @Test
+    void findAll() {
+        //List<String> sortParams = new ArrayList<>();
+        int page = 1;
+        int size = 3;
+        List<TagDto> expectedList = new ArrayList<>();
         List<Tag> tagListExpected = new ArrayList<>();
-        List<TagDto> tagDtoListExpected = new ArrayList<>();
-        Tag tag1 = new Tag();
-        tag1.setId(id1);
-        tag1.setName(tagName1);
-        Tag tag2 = new Tag();
-        tag2.setId(id2);
-        tag2.setName(tagName2);
-        tagListExpected.add(tag1);
-        tagListExpected.add(tag2);
+
         TagDto tagDto1 = new TagDto();
         TagDto tagDto2 = new TagDto();
-        tagDto1.setId(id1);
-        tagDto1.setName(tagName1);
-        tagDto2.setId(id2);
-        tagDto2.setName(tagName2);
-        tagDtoListExpected.add(tagDto1);
-        tagDtoListExpected.add(tagDto2);
-        when(tagDao.findAll()).thenReturn(tagListExpected);
-        when(tagMapper.mapEntityToDto(tag1)).thenReturn(tagDto1);
-        when(tagMapper.mapEntityToDto(tag2)).thenReturn(tagDto2);
-        List<TagDto> tagDtoListActual = tagService.findAll();
-        assertEquals(tagDtoListExpected, tagDtoListActual);
-        verify(tagDao, times(1)).findAll();
+        tagDto1.setId(1);
+        tagDto2.setId(2);
+        tagDto1.setName("tag1");
+        tagDto2.setName("tag2");
+        expectedList.add(tagDto1);
+        expectedList.add(tagDto2);
+
+        Tag tag1 = new Tag();
+        Tag tag2 = new Tag();
+        tag1.setId(1L);
+        tag2.setId(2L);
+        tag1.setName("tag1");
+        tag2.setName("tag2");
+        tagListExpected.add(tag1);
+        tagListExpected.add(tag2);
+
+        Mockito.doThrow().doNothing().when(paginationValidator).validateSize(size);
+        Mockito.doThrow().doNothing().when(paginationValidator).validatePageNumber(page);
+        Mockito.when(tagDao.findAll(page, size)).thenReturn(tagListExpected);
+        Mockito.when(tagMapper.mapEntityToDto(tag1)).thenReturn(tagDto1);
+        Mockito.when(tagMapper.mapEntityToDto(tag2)).thenReturn(tagDto2);
+        List<TagDto> actual = tagService.findAll(page, size);
+
+        Assertions.assertEquals(expectedList, actual);
+        Mockito.verify(paginationValidator, Mockito.times(1)).validateSize(size);
+        Mockito.verify(paginationValidator, Mockito.times(1)).validatePageNumber(page);
+        Mockito.verify(tagDao, Mockito.times(1)).findAll(page, size);
     }
 
     @Test
-    void delete() {
-    }
+    void countPages() {
+        int size = 50;
+        long expected = 3;
+        long recordsAmount = 122;
 
- */
+        Mockito.when(tagDao.findRecordsAmount()).thenReturn(recordsAmount);
+        Mockito.doThrow().doNothing().when(paginationValidator).validateSize(size);
+        long actual = tagService.countPages(size);
+
+        Assertions.assertEquals(expected, actual);
+        Mockito.verify(paginationValidator, Mockito.times(1)).validateSize(size);
+        Mockito.verify(tagDao, Mockito.times(1)).findRecordsAmount();
+    }
 }
